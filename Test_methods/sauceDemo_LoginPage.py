e6fe4a1cba547e4c7d4ff1159f9133028ffafd4c
Sreebalaji Chandrasekaran <37621721+sreebalajisree@users.noreply.github.com>
2022-09-27 12:25:25 +0530

    Some Code changes in the LoginPage test method as on 27 sep 2022
    Some Code changes in the LoginPage test method as on 27 sep 2022

--- Test_methods/sauceDemo_LoginPage.py
+++ Test_methods/sauceDemo_LoginPage.py
@@ -6,26 +6,35 @@
 from Config.config import TestData
 import pytest
 from playwright.sync_api import Playwright, sync_playwright
+from Test_methods.BasePage import Base
 
 
-class LoginPage(TestData):
+class LoginPage(Base):
+    """Creating variables for web-elements present in the login page"""
+    userName = "input[id=\"user-name\"]"
+    passWord = "input[id=\"password\"]"
+    loginButton = "[data-test=\"login-button\"]"
+    product_text = "text=Products"
 
-    def __init__(self, plywr: Playwright, base_url: str, headless=False):
+    """def __init__(self, plywr: Playwright, base_url: str, headless=False):
         self.browser = plywr.chromium.launch(headless=headless)
         self.context = self.browser.new_context()
         self.page = self.context.new_page()
-        self.base_url = base_url
+        self.base_url = base_url"""
 
-    def goto(self, endpoint: str, use_base_url=True):
+    def LoginPage(self, page):
+        self.page = page
+
+    """def goto(self, endpoint: str, use_base_url=True):
         if use_base_url:
             self.page.goto(self.base_url + endpoint)
         else:
             self.page.goto(endpoint)
 
-    """Go to base-URL"""
+        """'Go to base-URL'"""
 
     def webPage_Launch(self, url):
-        self.page.goto(url=url)
+        self.page.goto(url=url)"""
 
     """Test method to verify the URL title"""
 
@@ -34,7 +43,22 @@ class LoginPage(TestData):
         print("The URL page title is: ", title)
         return title
 
+    """Test method for performing login"""
+
+    """def login(self, username: str, password: str):
+        self.page.fill(self.userName, username)
+        self.page.fill(self.passWord, password)
+        self.page.click(self.loginButton)"""
+
+    """Test method to read homepage title (Products)"""
+
+    def homePage(self):
+        text = self.page.text_content(self.product_text)
+        print("The home page text is: ", text)
+        return text
+
     """Test teardown method"""
+
     def close_session(self):
         self.page.close()
         self.context.close()
